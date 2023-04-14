c2ee5a18b4099dcc5d1f75c875be7144b5b8780a
Yuxiang Zhang <yuxiangzhang@microsoft.com>
2023-04-14 12:06:42 +0800

    fix style

--- src/components/PromptResponseList/PromptResponseList.tsx
+++ src/components/PromptResponseList/PromptResponseList.tsx
@@ -1,8 +1,8 @@
-import React, {FC, useEffect, useRef} from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 import ChatGptImg from '../../img/chatgpt.png';
 import MyImg from '../../img/me.png';
 import ReactMarkdown from 'react-markdown';
-import {ResponseInterface} from "./response-interface";
+import { ResponseInterface } from "./response-interface";
 import hljs from 'highlight.js';
 import './PromptResponseList.css';
 
@@ -25,25 +25,27 @@ const PromptResponseList: FC<PromptResponseListProps> = ({ responseList }) => {
     <div className="prompt-response-list" ref={responseListRef}>
       {responseList.map((responseData) => (
         <div className={"response-container " + (responseData.selfFlag ? 'my-question' : 'chatgpt-response')} key={responseData.id}>
-          <img className="avatar-image" src={responseData.selfFlag ? MyImg : ChatGptImg} alt="avatar"/>
-          <div className={(responseData.error ? 'error-response ' : '') + "prompt-content"} id={responseData.id}>
-            { responseData.image &&
-                <img src={responseData.image} className="ai-image" alt="generated ai"/>
-            }
-            { responseData.response &&
-              <ReactMarkdown
-                children={responseData.response ?? ''}
-                components={{
-                  code({className, children}) {
-                    return(
-                      <code className={className}>
-                        {children}
-                      </code>
-                    )
-                  }
-                }}
-              />
-            }
+          <div className='response-content'>
+            <img className="avatar-image" src={responseData.selfFlag ? MyImg : ChatGptImg} alt="avatar" />
+            <div className={(responseData.error ? 'error-response ' : '') + "prompt-content"} id={responseData.id}>
+              {responseData.image &&
+                <img src={responseData.image} className="ai-image" alt="generated ai" />
+              }
+              {responseData.response &&
+                <ReactMarkdown
+                  children={responseData.response ?? ''}
+                  components={{
+                    code({ className, children }) {
+                      return (
+                        <code className={className}>
+                          {children}
+                        </code>
+                      )
+                    }
+                  }}
+                />
+              }
+            </div>
           </div>
         </div>
       ))}
